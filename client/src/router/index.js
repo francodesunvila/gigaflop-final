350e0fad932575431e60c561bc8dfdf081355639
Franco Desunvila <fdesuveron@hotmail.com>
2025-12-12 23:26:48 -0300

    deploy-13-rutas

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,54 +1,48 @@
-
+// src/router/index.js
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Menu from "../pages/Menu";
 import Clientes from "../pages/Clientes";
-import RutaProtegida from '../components/RutaProtegida';
+import RutaProtegida from "../components/RutaProtegida";
 import Home from "../pages/Home";
-import Productos from "../pages/Productos"; 
+import Productos from "../pages/Productos";
 import NuevaCotizacion from "../pages/NuevaCotizacion";
 import ResumenCotizacion from "../pages/ResumenCotizacion";
 import Configuracion from "../pages/Configuracion";
 import Dashboard from "../pages/Dashboard";
-import '../CSS/menu.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
-
-
-
+import "../CSS/menu.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {
     element: <RutaProtegida />, // rutas protegidas para cualquier usuario logueado
     children: [
-      { path: '/menu', element: <Menu /> },
-      { path: '/clientes', element: <Clientes /> },
-      { path: '/productos', element: <Productos /> },
-      { path: '/nuevacotizacion', element: <NuevaCotizacion /> },
-      { path: '/nuevacotizacion/:idCotizacion', element: <NuevaCotizacion /> },
-      { path: '/resumen-cotizacion', element: <ResumenCotizacion /> }
+      { path: "/menu", element: <Menu /> },
+      { path: "/clientes", element: <Clientes /> },
+      { path: "/productos", element: <Productos /> },
+      { path: "/nuevacotizacion", element: <NuevaCotizacion /> },
+      { path: "/nuevacotizacion/:idCotizacion", element: <NuevaCotizacion /> },
+      { path: "/resumen-cotizacion/:id", element: <ResumenCotizacion /> }, // 👈 ahora con ID
     ],
   },
   {
-    element: <RutaProtegida roles={["administrador"]} />, // 👈 solo admin
+    element: <RutaProtegida roles={["administrador"]} />, // solo admin
     children: [
-      { path: '/configuracion', element: <Configuracion /> }
+      { path: "/configuracion", element: <Configuracion /> },
     ],
   },
   {
-    element: <RutaProtegida roles={["administrador", "gerente"]} />, // 👈 solo admin/gerente
+    element: <RutaProtegida roles={["administrador", "gerente"]} />, // solo admin/gerente
     children: [
-      { path: '/dashboard', element: <Dashboard /> }
+      { path: "/dashboard", element: <Dashboard /> },
     ],
-  }
+  },
 ]);
-
-// { basename: '/gigaflop-pp3-app-react' });
